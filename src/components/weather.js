8dcdd8df692cb368e4f68252a3fd2fc6eff5eeb6
Mazurenko <vmazurenko913@gmail.com>
2020-08-06 18:03:10 +0300

    fix app

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-const Weather = props => (
-    <div className='infoWeath'>
-      {props.city && (
-        <div>
-          <p>
-            Местоположение: {props.city}, {props.country}
-          </p>
-          <p>Тепмература: {props.temp}</p>
-          <p>Давление: {props.pressure}</p>
-          <p>Заход солнца: {props.sunset}</p>
-        </div>
-      )}
-      <p className="error">{props.error}</p>
-    </div>
+const Weather = (props) => (
+  <div className="infoWeath">
+    {props.city && (
+      <div>
+        <p>
+          Местоположение: {props.city}, {props.country}
+        </p>
+        <p>Тепмература: {props.temp}</p>
+        <p>Давление: {props.pressure}</p>
+        <p>Заход солнца: {props.sunset}</p>
+      </div>
+    )}
+    <p className="error">{props.error}</p>
+  </div>
 );
 
 export default Weather;
